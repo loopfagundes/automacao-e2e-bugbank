46d086784c28599a0729b6ce9eb896c5dda89b25
Luciano <lof.fagundes@gmail.com>
2026-02-09 10:30:14 -0300

    chore: adicionou driver, steps e testou com sucesso

--- src/test/java/app/bugbank/runners/WebRunnerTest.java
+++ src/test/java/app/bugbank/runners/WebRunnerTest.java
@@ -1,4 +1,16 @@
 package app.bugbank.runners;
 
-public class WebRunnerTest {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "app.bugbank",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html"
+        },
+        monochrome = true
+)
+public class WebRunnerTest extends AbstractTestNGCucumberTests {
 }
