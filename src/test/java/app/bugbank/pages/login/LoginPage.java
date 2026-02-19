ad360d95290d58d6a4732111eb2701b19449a9f0
Luciano <lof.fagundes@gmail.com>
2026-02-19 16:14:24 -0300

    feat: adiciona os elementos na page

--- src/test/java/app/bugbank/pages/login/LoginPage.java
+++ src/test/java/app/bugbank/pages/login/LoginPage.java
@@ -13,4 +13,32 @@ public class LoginPage extends BasePage {
     public WebElement inputSenha() {
         return toBeClickable(By.xpath("//*[@id='__next'] //div[@class='card__login'] //input[@name='password']"));
     }
+
+    public WebElement acessarButton() {
+        return toBeClickable(By.xpath("//*[@id='__next'] //div[@class='card__login'] //button[@type='submit']"));
+    }
+
+    public WebElement bemVindoText() {
+        return visibilityOf(By.cssSelector("[class='home__ContainerText-sc-1auj767-7 iDA-Ddb']"));
+    }
+
+    public WebElement nomeDoUsuarioLogadoText() {
+        return visibilityOf(By.id("textName"));
+    }
+
+    public WebElement contaEDigitalDoUsuarioText() {
+        return visibilityOf(By.id("textAccountNumber"));
+    }
+
+    public WebElement saldoDoUsuarioText() {
+        return visibilityOf(By.id("textBalance"));
+    }
+
+    public WebElement sairButton() {
+        return toBeClickable(By.id("btnExit"));
+    }
+
+    public WebElement telaDeLoginText() {
+        return visibilityOf(By.cssSelector("[class='pages__Title-sc-1ee1f2s-4 cFmqIK']"));
+    }
 }
