ff9fda67c54adbd10681b56da914108353714f63
Luciano <lof.fagundes@gmail.com>
2026-02-19 16:14:51 -0300

    feat: adiciona acao na interactions

--- src/test/java/app/bugbank/interactions/login/LoginInteractions.java
+++ src/test/java/app/bugbank/interactions/login/LoginInteractions.java
@@ -2,13 +2,43 @@ package app.bugbank.interactions.login;
 
 import app.bugbank.pages.login.LoginPage;
 
+import static app.bugbank.widgets.Element.*;
+
 public class LoginInteractions extends LoginPage {
 
-    public void preencherInputDoEmail() {
-        inputEmail().sendKeys("GOL!");
+    public void preencherInputDoEmail(String usuario) {
+        sendKeysJson(inputEmail(), "Login", usuario);
     }
 
     public void preencherInputDaSenha() {
-        inputSenha().sendKeys("SENHAAAAAAAAA");
+        sendKeysJson(inputSenha(), "Login", "Senha");
+    }
+
+    public void clicaParaAcessar() {
+        click(acessarButton());
+    }
+
+    public void validaTelaLogadobemVindo() {
+        System.out.println(">>>>>>> BEM VINDO >" + bemVindoText().getText());
+    }
+
+    public void validaNomeDoUsuarioLogado() {
+        System.out.println(">>>>>>>> NOME > " + nomeDoUsuarioLogadoText().getText());
+    }
+
+    public void validaAContaEDigitalDoUsuario() {
+        System.out.println(">>>>>>>> CONTA E DIGITAL > " + contaEDigitalDoUsuarioText().getText());
+    }
+
+    public void validaSaldoDoUsuario() {
+        System.out.println(">>>>>>>> SALDO > " + saldoDoUsuarioText().getText());
+    }
+
+    public void clicaSairDaTelaDeLogin() {
+        click(sairButton());
+    }
+
+    public void validaATelaDeLogin() {
+        System.out.println(">>>>>>>>> LOGIN > " + telaDeLoginText().getText());
     }
 }
